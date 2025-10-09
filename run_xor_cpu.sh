f2a3ae1a41149ed98589488188f78c77e647d107
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-09 15:53:31 +0200

    Script for jax cpu

---
+++ run_xor_cpu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#script to be run server-side
+
+# install requirements, then execute script with given arguments
+
+cd /project_antwerp/code/LGNEAT
+# pip install wfdb
+python -m pip install -U pip
+
+pip install -U "jax"
+pip install git+https://github.com/EMI-Group/tensorneat.git
+
+
+
+
+
+
+#for 1 run:
+python3 "xor.py" 
