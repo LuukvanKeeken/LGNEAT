f7973ed032354bf9e536517810e8a184e51fead7
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-14 22:01:57 +0200

    10k generations

--- xor_hyperneat_mlp_two_inputs.py
+++ xor_hyperneat_mlp_two_inputs.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=XOR3d(),
-        generation_limit=1000
+        generation_limit=10000
     )
 
     # initialize state
