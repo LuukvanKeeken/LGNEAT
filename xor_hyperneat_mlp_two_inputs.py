fac7381bc7ea720fd6b09bcce10e23af10c5c6a8
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-14 21:47:33 +0200

    Trying slightly larger network with LEO

--- xor_hyperneat_mlp_two_inputs.py
+++ xor_hyperneat_mlp_two_inputs.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
 
     algorithm=HyperNEATFeedForwardCustTwoInputs(
             substrate=MLPSubstrateLEO(
-                layers=[4, 3, 3, 1],
+                layers=[4, 50, 50, 1],
             ),
             neat=NEAT(
-                pop_size=10,
+                pop_size=1000,
                 species_size=20,
                 survival_threshold=0.01,
                 genome=DefaultGenome(
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=XOR3d(),
-        generation_limit=20
+        generation_limit=1000
     )
 
     # initialize state
