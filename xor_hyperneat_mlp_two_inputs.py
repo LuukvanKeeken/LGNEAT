2250152feed922571a6ac90a0dd3f641eb87bbab
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-14 22:03:16 +0200

    small network

--- xor_hyperneat_mlp_two_inputs.py
+++ xor_hyperneat_mlp_two_inputs.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     algorithm=HyperNEATFeedForwardCustTwoInputs(
             substrate=MLPSubstrateLEO(
-                layers=[4, 50, 50, 1],
+                layers=[4, 5, 5, 1],
             ),
             neat=NEAT(
                 pop_size=1000,
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=XOR3d(),
-        generation_limit=10000
+        generation_limit=100
     )
 
     # initialize state
