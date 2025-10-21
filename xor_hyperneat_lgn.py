cead844b97d46eaa029264c48d6f5b0306a7cd92
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-21 21:13:57 +0200

    [3, 100, 100, 1]

--- xor_hyperneat_lgn.py
+++ xor_hyperneat_lgn.py
@@ -29,7 +29,7 @@ if not os.path.exists(f"results/{timestamp}_lgn/imgs"):
 
 if __name__ == "__main__":
 
-    layers = [3, 50, 50, 1]
+    layers = [3, 100, 100, 1]
 
     neat_inputs = 4
     neat_outputs = 3
