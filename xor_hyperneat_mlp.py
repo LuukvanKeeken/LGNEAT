e97d52419b2cc639756084d3ca960aa8c0e861b1
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-21 21:11:52 +0200

    Correctly saving results for mlp

--- xor_hyperneat_mlp.py
+++ xor_hyperneat_mlp.py
@@ -9,23 +9,45 @@ import jax.numpy as jnp
 from tensorneat.src.tensorneat.problem.func_fit import XOR3d
 
 import time
+import os
+import contextlib
 
 start_time = time.time()
 
+# Check if results directory exists, if not, create it
+if not os.path.exists("results"):
+    os.makedirs("results")
+
+# Create directory in results with current timestamp,
+# if it does not exist
+timestamp = time.strftime("%Y%m%d-%H%M%S")
+if not os.path.exists(f"results/{timestamp}_mlp"):
+    os.makedirs(f"results/{timestamp}_mlp")
+
+# Create a directory in results/timestamp for images
+if not os.path.exists(f"results/{timestamp}_mlp/imgs"):
+    os.makedirs(f"results/{timestamp}_mlp/imgs")
+
 if __name__ == "__main__":
+
+    layers = [4, 2, 1]
+
+    neat_inputs = 4
+    neat_outputs = 1
+    neat_hidden_layers = ()
 
     algorithm=HyperNEATFeedForwardCust(
             substrate=MLPSubstrate(
-                layers=[4, 50, 50, 1],
+                layers=layers,
             ),
             neat=NEAT(
-                pop_size=1000,
+                pop_size=10,
                 species_size=20,
                 survival_threshold=0.01,
                 genome=DefaultGenome(
-                    num_inputs=4,  # size of query coors
-                    num_outputs=1,
-                    init_hidden_layers=(),
+                    num_inputs=neat_inputs,  # size of query coors
+                    num_outputs=neat_outputs,
+                    init_hidden_layers=neat_hidden_layers,
                     output_transform=ACT.tanh,
                 ),
             ),
@@ -36,36 +58,50 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=XOR3d(),
-        generation_limit=10000
+        generation_limit=2
     )
 
-    # initialize state
-    state = pipeline.setup()
-    # print(state)
-    # run until terminate
-    filename = f"./imgs/current_gen.txt"
-    state, best = pipeline.auto_run(state, filename)
+    with open(f"results/{timestamp}_mlp/settings.txt", "w") as f_settings:
+        f_settings.write(f"Generations: {pipeline.generation_limit}\n")
+        f_settings.write(f"Population size: {algorithm.neat.pop_size}\n")
+        f_settings.write(f"Substrate layers: {layers}\n")
+        f_settings.write(f"Species: {algorithm.neat.species_controller.species_size}\n")
+        f_settings.write(f"NEAT init. shape {neat_inputs, neat_hidden_layers, neat_outputs}\n")
+        f_settings.write(f"Seed: {pipeline.seed}\n")
+        f_settings.write(f"Problem task: {pipeline.problem.__class__.__name__}\n")
 
-    print(f"Total time: {time.time() - start_time:.2f} seconds")
-    print(f"Approximate time/generation: {(time.time() - start_time)/pipeline.generation_limit:.2f} seconds\n")
+    print("Starting training ...")
+    with open(f"results/{timestamp}_mlp/log.txt", "w") as f_log:
+        with contextlib.redirect_stdout(f_log):
+            # initialize state
+            state = pipeline.setup()
+            # print(state)
+            # run until terminate
+            filename = f"results/{timestamp}_mlp/current_gen.txt"
+            state, best = pipeline.auto_run(state, filename)
+
+    print("Finished training.")
+
+    with open(f"results/{timestamp}_mlp/best.txt", "w") as f_best:
+        with contextlib.redirect_stdout(f_best):
+            print(f"Total time: {time.time() - start_time:.2f} seconds")
+            print(f"Approximate time/generation: {(time.time() - start_time)/pipeline.generation_limit:.2f} seconds\n")
 
 
-    start_time_test = time.time()
-    # show result
-    for i in range(20):
-        pipeline.show(state, best)
-    print(f"Testing time: {time.time() - start_time_test} seconds\n")
-    print(f"Average time per test: {(time.time() - start_time_test)/20} seconds\n")
+            start_time_test = time.time()
+            # show result
+            pipeline.show(state, best)
+            print(f"Testing time: {time.time() - start_time_test} seconds\n")
 
-    # visualize the best individual
-    network = algorithm.neat.genome.network_dict(state, *best)
-    print(algorithm.neat.genome.repr(state, *best))
-    algorithm.neat.genome.visualize(network, save_path="./imgs/xor_CPPN_network_mlp.svg")
+            # visualize the best individual
+            network = algorithm.neat.genome.network_dict(state, *best)
+            print(algorithm.neat.genome.repr(state, *best))
+            algorithm.neat.genome.visualize(network, save_path=f"results/{timestamp}_mlp/imgs/xor_CPPN_network_mlp.svg")
 
-    transformed = algorithm.transform(state, best)
-    seqs, h_nodes, h_conns, u_conns = transformed
-    hyper_network = algorithm.hyper_genome.network_dict(state, h_nodes, h_conns)
-    print(algorithm.hyper_genome.repr(state, h_nodes, h_conns))
-    algorithm.hyper_genome.visualize(hyper_network, save_path="./imgs/xor_hyperneat_network_mlp.svg")
+            transformed = algorithm.transform(state, best)
+            seqs, h_nodes, h_conns, u_conns = transformed
+            hyper_network = algorithm.hyper_genome.network_dict(state, h_nodes, h_conns)
+            print(algorithm.hyper_genome.repr(state, h_nodes, h_conns))
+            algorithm.hyper_genome.visualize(hyper_network, save_path=f"results/{timestamp}_mlp/imgs/xor_hyperneat_network_mlp.svg")
 
     
