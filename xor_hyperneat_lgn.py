25f77e035acce1028bb05c8636576b3ed2429b37
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-22 11:52:25 +0200

    Start with 2 hidden nodes in neat

--- xor_hyperneat_lgn.py
+++ xor_hyperneat_lgn.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 
     neat_inputs = 4
     neat_outputs = 3
-    neat_hidden_layers = ()
+    neat_hidden_layers = (2,)
 
     algorithm=HyperNEATFeedForwardLGN(
             substrate=LGNSubstrateLEO(
