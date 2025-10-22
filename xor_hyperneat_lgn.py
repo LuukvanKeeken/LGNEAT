1b99afdb78d473cc1240f3d622bac9f2f221543e
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-22 14:58:27 +0200

    Testing with correct gate type selection

--- xor_hyperneat_lgn.py
+++ xor_hyperneat_lgn.py
@@ -40,10 +40,10 @@ if __name__ == "__main__":
                 layers=layers,
             ),
             neat=NEAT(
-                pop_size=500,
+                pop_size=100,
                 species_size=20,
                 survival_threshold=0.01,
-                species_elitism=0,
+                species_elitism=1,
                 genome=DefaultGenomeCPPN(
                     num_inputs=neat_inputs,  # size of query coors
                     num_outputs=neat_outputs,
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=XOR3d(),
-        generation_limit=100000,
+        generation_limit=50000,
         seed=3
     )
 
