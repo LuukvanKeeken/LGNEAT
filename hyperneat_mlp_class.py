9058997f471efa454302856f9deb782f6a0d06f1
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-23 15:28:46 +0200

    hn mlp binary task 0.2 limit

--- hyperneat_mlp_class.py
+++ hyperneat_mlp_class.py
@@ -30,7 +30,7 @@ if not os.path.exists(f"results/{timestamp}_mlp/imgs"):
 
 if __name__ == "__main__":
 
-    layers = [7, 10, 10, 2, 1]
+    layers = [7, 10, 10, 1]
 
     neat_inputs = 4
     neat_outputs = 1
@@ -52,7 +52,7 @@ if __name__ == "__main__":
                 ),
             ),
             activation=ACT.tanh,
-            output_transform=ACT.identity,
+            output_transform=ACT.sigmoid,
         )
     
     # Check if input coordinates are inside a circle
@@ -97,7 +97,8 @@ if __name__ == "__main__":
         algorithm=algorithm,
         problem=even_problem,
         fitness_target=-0.02,
-        generation_limit=3000,
+        generation_limit=100000,
+        fitness_target=-0.2,
         seed=3
     )
 
