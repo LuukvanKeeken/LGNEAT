cc79896d28f760a8c49f1046ac37d94d187e7ea3
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-27 17:31:07 +0100

    0.01

--- hyperneat_mlp_class.py
+++ hyperneat_mlp_class.py
@@ -97,8 +97,8 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=even_problem,
-        fitness_target=-0.1,
-        generation_limit=20000,
+        fitness_target=-0.01,
+        generation_limit=100000,
         seed=4
     )
 
