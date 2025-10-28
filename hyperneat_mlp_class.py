973f87a5b71d0af7d71676ac0d7c660a22d33595
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-28 17:43:33 +0100

    10k gens

--- hyperneat_mlp_class.py
+++ hyperneat_mlp_class.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
         algorithm=algorithm,
         problem=even_problem,
         fitness_target=-0.1,
-        generation_limit=100000,
+        generation_limit=10000,
         seed=4
     )
 
