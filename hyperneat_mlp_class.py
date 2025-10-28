35cb9eef33a3d7a27bcafec0da2d14b2ff04880e
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-28 11:58:15 +0100

    Different seed

--- hyperneat_mlp_class.py
+++ hyperneat_mlp_class.py
@@ -98,8 +98,8 @@ if __name__ == "__main__":
         algorithm=algorithm,
         problem=even_problem,
         fitness_target=-0.01,
-        generation_limit=10000,
-        seed=4
+        generation_limit=3000,
+        seed=5
     )
 
 
