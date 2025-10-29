1107afd179c6655878e141d31857df96ce0c2084
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-29 19:24:15 +0100

    Include square activation

--- tensorneat/src/tensorneat/genome/default_cppn.py
+++ tensorneat/src/tensorneat/genome/default_cppn.py
@@ -33,9 +33,7 @@ class DefaultGenomeCPPN(DefaultGenome):
         num_outputs: int,
         max_nodes=50,
         max_conns=100,
-        node_gene=DefaultNode(
-            activation_options=[ACT.gaussian, ACT.sigmoid, ACT.sin, ACT.abs],
-            aggregation_options=[AGG.sum, AGG.product]),
+        node_gene=DefaultNode(activation_options=[ACT.gaussian, ACT.sigmoid, ACT.sin, ACT.abs, ACT.square]),
         conn_gene=DefaultConn(),
         mutation=DefaultMutation(),
         crossover=DefaultCrossover(),
