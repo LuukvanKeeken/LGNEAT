6b385dc3ff5542530819567929cdad138e64fa78
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-29 17:48:58 +0100

    100k sum and prod

--- hyperneat_lgn_class.py
+++ hyperneat_lgn_class.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=even_problem,
-        generation_limit=10000,
+        generation_limit=100000,
         seed=3
     )
 
