394fb405e07ecc79adf6f2b6187fb07a591a4188
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-29 20:44:06 +0100

    40k

--- hyperneat_lgn_class.py
+++ hyperneat_lgn_class.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=even_problem,
-        generation_limit=10000,
+        generation_limit=40000,
         seed=3
     )
 
