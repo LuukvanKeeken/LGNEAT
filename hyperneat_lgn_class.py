d3ef8f5e3c5fc3871af2a974c6aa39078fdb926c
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-29 23:15:51 +0100

    6 10 10 10 10

--- hyperneat_lgn_class.py
+++ hyperneat_lgn_class.py
@@ -29,7 +29,7 @@ if not os.path.exists(f"results/{timestamp}_lgn/imgs"):
 
 if __name__ == "__main__":
 
-    layers = [6, 20, 20, 20]
+    layers = [6, 10, 10, 10, 10]
 
     neat_inputs = 4
     neat_outputs = 5
