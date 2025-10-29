537a086ed4e637cb6b9a8ebe01bfb5b2b9d3968e
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-29 19:50:30 +0100

    30k gens

--- hyperneat_mlp_class.py
+++ hyperneat_mlp_class.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=even_problem,
-        generation_limit=10000,
+        generation_limit=30000,
         seed=3
     )
 
