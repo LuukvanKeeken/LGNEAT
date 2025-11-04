f8808cd6c25ca5ebd6ba3e339cd859afe5eaf3a7
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-11-04 22:19:55 +0100

    even smaller substrate

--- hyperneat_lgn_class.py
+++ hyperneat_lgn_class.py
@@ -29,7 +29,7 @@ if not os.path.exists(f"results/{timestamp}_lgn/imgs"):
 
 if __name__ == "__main__":
 
-    layers = [6, 4, 4, 4]
+    layers = [6, 4, 4]
 
     neat_inputs = 4
     neat_outputs = 17
@@ -77,8 +77,8 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=even_problem,
-        fitness_target=-0.01,
-        generation_limit=50000,
+        fitness_target=-0.127,
+        generation_limit=10000,
         seed=3
     )
 
