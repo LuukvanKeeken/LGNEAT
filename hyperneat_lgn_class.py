bf55aa7f533f11765fd07627b0562a5c088aaf8b
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-11-04 21:56:05 +0100

    Much smaller substrate

--- hyperneat_lgn_class.py
+++ hyperneat_lgn_class.py
@@ -29,7 +29,7 @@ if not os.path.exists(f"results/{timestamp}_lgn/imgs"):
 
 if __name__ == "__main__":
 
-    layers = [6, 10, 10, 10, 10]
+    layers = [6, 4, 4, 4]
 
     neat_inputs = 4
     neat_outputs = 17
