f586f3075fa983b29165ec03e4c3c508fdb582a9
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-11-04 22:10:54 +0100

    50k gens

--- hyperneat_lgn_class.py
+++ hyperneat_lgn_class.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
         algorithm=algorithm,
         problem=even_problem,
         fitness_target=-0.01,
-        generation_limit=10000,
+        generation_limit=50000,
         seed=3
     )
 
