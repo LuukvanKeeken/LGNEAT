32f1013450336d664835ee742baa531810097940
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-11-05 14:39:35 +0100

    Thin network

--- hyperneat_lgn_class.py
+++ hyperneat_lgn_class.py
@@ -29,7 +29,7 @@ if not os.path.exists(f"results/{timestamp}_lgn/imgs"):
 
 if __name__ == "__main__":
 
-    layers = [6, 10, 10, 10, 10]
+    layers = [6, 4, 4, 4, 4, 6]
 
     neat_inputs = 4
     neat_outputs = 17
@@ -77,8 +77,8 @@ if __name__ == "__main__":
     pipeline = Pipeline(
         algorithm=algorithm,
         problem=even_problem,
-        fitness_target=-0.01,
-        generation_limit=10000,
+        fitness_target=-0.05,
+        generation_limit=100000,
         seed=3
     )
 
