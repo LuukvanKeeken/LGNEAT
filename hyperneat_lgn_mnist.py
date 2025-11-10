56cfa192e6b75045ee26af5184f978ae047d5e9e
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-11-10 11:48:33 +0100

    halved population

--- hyperneat_lgn_mnist.py
+++ hyperneat_lgn_mnist.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
                     layers=layers,
                 ),
                 neat=NEAT(
-                    pop_size=1000,
+                    pop_size=500,
                     species_size=20,
                     survival_threshold=0.01,
                     species_elitism=1,
