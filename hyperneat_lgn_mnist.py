a08c840a601baad2edc723d00b426cfe331773ff
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-11-10 13:46:18 +0100

    halved cols

--- hyperneat_lgn_mnist.py
+++ hyperneat_lgn_mnist.py
@@ -112,7 +112,7 @@ if __name__ == "__main__":
                     layers=layers,
                 ),
                 neat=NEAT(
-                    pop_size=10,
+                    pop_size=1000,
                     species_size=20,
                     survival_threshold=0.01,
                     species_elitism=1,
