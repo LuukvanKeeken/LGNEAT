5524adff242ffa5c951173eec8fb0ef64d70ff81
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-11-10 11:45:48 +0100

    baseline

--- hyperneat_lgn_mnist.py
+++ hyperneat_lgn_mnist.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
             algorithm=algorithm,
             problem=mnist,
             fitness_target=-0.02,
-            generation_limit=100000,
+            generation_limit=20,
             seed=i,
             is_save=True,
             save_dir=f"results/{timestamp}_lgn",
