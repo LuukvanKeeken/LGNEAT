40638b4770aab3bcfa6549458eff67d26bfd1505
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-11-10 13:40:59 +0100

    Half rows

--- hyperneat_lgn_mnist.py
+++ hyperneat_lgn_mnist.py
@@ -49,6 +49,7 @@ def pil_to_jax_array_and_binarize(pil_img):
     np_img = np.array(pil_img, dtype=np.float32)
     # Now binarize the image: pixels > 127 become 1, else 0
     np_img = (np_img > 127).astype(np.float32)
+    np_img = np_img[:14, :]  # Keep only the first 14 rows
     return jnp.array(np_img)
 
 def label_to_one_hot(label):
@@ -100,7 +101,7 @@ if __name__ == "__main__":
     num_runs = 1
     for i in range(num_runs):
 
-        layers = [28, 12]
+        layers = [28, 10, 10, 12]
 
         neat_inputs = 4
         neat_outputs = 17
