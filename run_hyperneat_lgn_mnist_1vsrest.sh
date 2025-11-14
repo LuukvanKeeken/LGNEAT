b998576361d546d6257862cbba30960a0816b491
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-11-14 14:53:03 +0100

    Trying 2 vs rest classification

---
+++ run_hyperneat_lgn_mnist_1vsrest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#script to be run server-side
+
+# install requirements, then execute script with given arguments
+
+cd /project_antwerp/code/LGNEAT
+# pip install wfdb
+python -m pip install -U pip
+
+pip install -U "jax[cuda12]"
+
+
+
+#for 1 run:
+python3 "hyperneat_lgn_mnist_1vsrest.py" 
